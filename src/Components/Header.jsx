3f06ab31d8a9198ff50f2ff54e70dc5a9f8bef42
rossa <fashionfish@naver.com>
2021-07-16 17:32:10 +0900

    #2.2 react router part two

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/tv">Tv</a>
+      </li>
+      <li>
+        <a href="/search">Search</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Home from 'Routes/Home';
-import Movie from 'Routes/Movie';
 import Search from 'Routes/Search';
 import Tv from 'Routes/Tv';
 
 const Router = () => {
   return (
-    <HashRouter>
-      <Route exact path="/" component={Home} />
-      <Route path="/movie" component={Movie} />
-      <Route path="/search" component={Search} />
-      <Route path="/tv" component={Tv} />
-    </HashRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/search" component={Search} />
+        <Route path="/tv" component={Tv} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
