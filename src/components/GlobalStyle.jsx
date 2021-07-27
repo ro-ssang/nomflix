6e43dd152022f75c1d013f793599b9d6016554a7
rossa <fashionfish@naver.com>
2021-07-27 22:54:31 +0900

    Add globalstyle - reset css, theme
    - reset CSS
    - reset <a> tag
    - add theme colors

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export const theme = {
+  $black: '#141414',
+  $white: '#fff',
+  $red: '#e50914',
+};
+
+export default createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.$black};
+    color: ${(props) => props.theme.$white};
+    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,15 +1,20 @@
+import GlobalStyle, { theme } from '@components/GlobalStyle';
 import HomeContainer from '@pages/Home';
 import SearchContainer from '@pages/Search';
 import TvContainer from '@pages/Tv';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 export default () => {
   return (
-    <Router>
-      <Route path="/" exact component={HomeContainer} />
-      <Route path="/tv" component={TvContainer} />
-      <Route path="/search" component={SearchContainer} />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Route path="/" exact component={HomeContainer} />
+        <Route path="/tv" component={TvContainer} />
+        <Route path="/search" component={SearchContainer} />
+      </Router>
+    </ThemeProvider>
   );
 };
