bb0c04ea77007c33682f10ef0c8793e641fc602a
rossa <fashionfish@naver.com>
2021-07-27 22:57:12 +0900

    Add header component

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,8 +1,14 @@
+import Header from '@components/Header';
 import React from 'react';
 import styled from 'styled-components';
 
 const MainWrapper = styled.main``;
 
 export default ({ children }) => {
-  return <MainWrapper>{children}</MainWrapper>;
+  return (
+    <MainWrapper>
+      <Header />
+      {children}
+    </MainWrapper>
+  );
 };
