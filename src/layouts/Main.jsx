66b64717c68305b726d23577f99a0a632ec49277
rossa <fashionfish@naver.com>
2021-07-29 22:56:28 +0900

    Design main component

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,7 +2,13 @@ import Header from '@components/Header';
 import React from 'react';
 import styled from 'styled-components';
 
-const MainWrapper = styled.main``;
+const MainWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 32px;
+  padding-bottom: 48px;
+`;
 
 export default ({ children }) => {
   return (
