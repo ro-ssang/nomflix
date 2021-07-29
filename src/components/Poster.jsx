12e2ce9aeda9cedd68860f07a62c68932ec39693
rossa <fashionfish@naver.com>
2021-07-29 22:56:07 +0900

    Design poster component

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -2,17 +2,61 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Backdrop = styled.img``;
-const Title = styled.h3``;
+const Backdrop = styled.img`
+  width: 100%;
+  height: 100%;
+  transition: transform 0.25s ease-out;
+`;
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 0 4px;
+  background-color: rgba(0, 0, 0, 0.6);
+  opacity: 0;
+`;
+const Title = styled.h3`
+  margin-bottom: 8px;
+  width: 100%;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 1.1;
+  text-align: center;
+`;
 const Rating = styled.span``;
+const PosterWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  box-shadow: 3px 4px 10px ${(props) => props.theme.$lightBlack};
+  &:hover,
+  &:focus {
+    ${Backdrop} {
+      transform: scale(1.1);
+    }
+    ${Info} {
+      opacity: 1;
+    }
+  }
+`;
 
 const Poster = ({ title, imgUrl, rating }) => {
   return (
-    <>
+    <PosterWrapper>
       <Backdrop src={`https://image.tmdb.org/t/p/w300${imgUrl}`} alt={title} />
-      <Title>{title}</Title>
-      <Rating>⭐{rating}/10</Rating>
-    </>
+      <Info>
+        <Title>{title}</Title>
+        <Rating>⭐{rating}/10</Rating>
+      </Info>
+    </PosterWrapper>
   );
 };
 
