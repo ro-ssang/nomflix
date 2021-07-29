7fabca45baff7b0c9d168ee07a649649253b286d
rossa <fashionfish@naver.com>
2021-07-29 20:13:58 +0900

    Update home component - prop types

--- src/pages/Home/HomePresenter.jsx
+++ src/pages/Home/HomePresenter.jsx
@@ -1,10 +1,11 @@
 import MainWrapper from '@layouts/Main';
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const HomeWrapper = styled.section``;
 
-const HomePresenter = () => {
+const HomePresenter = ({ loading, nowPlaying, upcoming, popular, error }) => {
   return (
     <MainWrapper>
       <HomeWrapper>Home</HomeWrapper>
@@ -13,3 +14,33 @@ const HomePresenter = () => {
 };
 
 export default HomePresenter;
+
+HomePresenter.propType = {
+  loading: PropTypes.bool.isRequired,
+  nowPlaying: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  upcoming: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  popular: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  error: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }),
+};
