52cfbc870c7f93067a502dfd6f624face7794ce7
rossa <fashionfish@naver.com>
2021-08-02 20:12:24 +0900

    Update detail container component - prop types

--- src/pages/Detail/DetailContainer.jsx
+++ src/pages/Detail/DetailContainer.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DetailPresenter from './DetailPresenter';
 
-export default () => {
+const DetailContainer = ({
+  location: { pathname },
+  match: {
+    params: { id },
+  },
+}) => {
   return <DetailPresenter />;
+};
+
+export default DetailContainer;
+
+DetailContainer.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }),
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }),
 };
