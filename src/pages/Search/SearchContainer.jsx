4c965dac81cc8874c513b5d7ae2b7ca3641d9e03
rossa <fashionfish@naver.com>
2021-08-02 15:11:47 +0900

    Update search component - fetch movies and tv shows data

--- src/pages/Search/SearchContainer.jsx
+++ src/pages/Search/SearchContainer.jsx
@@ -1,6 +1,56 @@
-import React from 'react';
+import { movieApi, tvApi } from '@api/movieDatabase';
+import React, { useCallback, useState } from 'react';
 import SearchPresenter from './SearchPresenter';
 
 export default () => {
-  return <SearchPresenter />;
+  const [term, setTerm] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [shows, setShows] = useState([]);
+  const [error, setError] = useState(null);
+
+  const onChange = useCallback(
+    (e) => {
+      setTerm(e.target.value);
+    },
+    [term],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const {
+            data: { results: movies },
+          } = await movieApi.search(term);
+          const {
+            data: { results: shows },
+          } = await tvApi.search(term);
+          setMovies(movies);
+          setShows(shows);
+        } catch (err) {
+          setError(err);
+        } finally {
+          setLoading(false);
+          setTerm('');
+        }
+      };
+      fetchData();
+    },
+    [term],
+  );
+
+  return (
+    <SearchPresenter
+      term={term}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      loading={loading}
+      movies={movies}
+      shows={shows}
+      error={error}
+    />
+  );
 };
