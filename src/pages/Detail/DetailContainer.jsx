8dd1902d9170738335b52f05ac870be983ece6ab
rossa <fashionfish@naver.com>
2021-08-02 19:47:58 +0900

    Add detail component

---
+++ src/pages/Detail/DetailContainer.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import DetailPresenter from './DetailPresenter';
+
+export default () => {
+  return <DetailPresenter />;
+};
---
+++ src/pages/Detail/DetailPresenter.jsx
@@ -0,0 +1,15 @@
+import MainWrapper from '@layouts/Main';
+import React from 'react';
+import styled from 'styled-components';
+
+const DetailWrapper = styled.div``;
+
+const DetailPresenter = () => {
+  return (
+    <MainWrapper>
+      <DetailWrapper>Detail page</DetailWrapper>
+    </MainWrapper>
+  );
+};
+
+export default DetailPresenter;
