99e31c0d6df5d248c3dcd650ea35c32c74ae6def
rossa <fashionfish@naver.com>
2021-08-02 15:35:42 +0900

    Update search component - prop types

--- src/pages/Search/SearchPresenter.jsx
+++ src/pages/Search/SearchPresenter.jsx
@@ -1,10 +1,11 @@
 import MainWrapper from '@layouts/Main';
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const SearchWrapper = styled.section``;
 
-const SearchPresenter = () => {
+const SearchPresenter = ({ term, onChange, onSubmit, loading, movies, shows, error }) => {
   return (
     <MainWrapper>
       <SearchWrapper>Search</SearchWrapper>
@@ -13,3 +14,29 @@ const SearchPresenter = () => {
 };
 
 export default SearchPresenter;
+
+SearchPresenter.propTypes = {
+  term: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  shows: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      poster_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  error: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }),
+};
