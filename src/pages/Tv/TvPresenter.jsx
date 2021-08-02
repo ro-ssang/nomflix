77e4a0fd13e7d3874b97fca0ae942e0b2f76592e
rossa <fashionfish@naver.com>
2021-08-02 13:55:19 +0900

    Update tv component - prop types

--- src/pages/Tv/TvPresenter.jsx
+++ src/pages/Tv/TvPresenter.jsx
@@ -1,10 +1,11 @@
 import MainWrapper from '@layouts/Main';
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const TvWrapper = styled.section``;
 
-const TvPresenter = () => {
+const TvPresenter = ({ loading, airingToday, popular, topRated, error }) => {
   return (
     <MainWrapper>
       <TvWrapper>TV</TvWrapper>
@@ -13,3 +14,34 @@ const TvPresenter = () => {
 };
 
 export default TvPresenter;
+
+TvPresenter.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  airingToday: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      backdrop_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  popular: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      backdrop_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  topRated: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      backdrop_path: PropTypes.string,
+      vote_average: PropTypes.number.isRequired,
+    }),
+  ),
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }),
+};
