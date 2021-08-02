efe8a57656f47c19063f2d32385c5264b48e8a99
rossa <fashionfish@naver.com>
2021-08-02 20:35:24 +0900

    Update detail presenter component - prop types

--- src/pages/Detail/DetailPresenter.jsx
+++ src/pages/Detail/DetailPresenter.jsx
@@ -1,10 +1,11 @@
 import MainWrapper from '@layouts/Main';
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const DetailWrapper = styled.div``;
 
-const DetailPresenter = () => {
+const DetailPresenter = ({ loading, movie, show, error }) => {
   return (
     <MainWrapper>
       <DetailWrapper>Detail page</DetailWrapper>
@@ -13,3 +14,38 @@ const DetailPresenter = () => {
 };
 
 export default DetailPresenter;
+
+DetailPresenter.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string,
+    backdrop_path: PropTypes.string,
+    genres: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+      }),
+    ),
+    runtime: PropTypes.number.isRequired,
+    release_date: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired,
+  }),
+  show: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    poster_path: PropTypes.string,
+    backdrop_path: PropTypes.string,
+    genres: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+      }),
+    ),
+    episode_runtime: PropTypes.arrayOf(PropTypes.number),
+    first_air_date: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired,
+  }),
+  error: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }),
+};
