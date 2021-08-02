8b0f6d2a2e77823dcffd374c6ef2a708a346ef2c
rossa <fashionfish@naver.com>
2021-08-02 20:13:32 +0900

    Update detail component - fetch data

--- src/pages/Detail/DetailContainer.jsx
+++ src/pages/Detail/DetailContainer.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { movieApi, tvApi } from '@api/movieDatabase';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import DetailPresenter from './DetailPresenter';
 
@@ -8,7 +9,41 @@ const DetailContainer = ({
     params: { id },
   },
 }) => {
-  return <DetailPresenter />;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+  const [show, setShow] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (pathname.startsWith('/movies')) {
+      const fetchMovieData = async () => {
+        try {
+          const { data } = await movieApi.detail(id);
+          setMovie(data);
+        } catch (err) {
+          setError(err);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchMovieData();
+    }
+    if (pathname.startsWith('/shows')) {
+      const fetchShowData = async () => {
+        try {
+          const { data } = await tvApi.detail(id);
+          setShow(data);
+        } catch (err) {
+          setError(err);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchShowData();
+    }
+  }, []);
+
+  return <DetailPresenter loading={loading} movie={movie} show={show} error={error} />;
 };
 
 export default DetailContainer;
