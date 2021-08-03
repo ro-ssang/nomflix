daef58d0c8876a11435cc701305a4279505cfb92
rossa <fashionfish@naver.com>
2021-08-03 23:44:42 +0900

    Deployment

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              ['@babel/preset-env', { targets: ['>0.2%', 'not dead', 'not op_mini all'] }],
+              '@babel/preset-react',
+            ],
           },
         },
       },
